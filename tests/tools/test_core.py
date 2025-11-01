00b32f37a762ce234907a8e5310068bb96cc7eb1
jayant <jayant746@gmail.com>
2025-11-01 17:45:27 -0400

    ENH: Schema comparison tool with happy path only

--- tests/tools/test_core.py
+++ tests/tools/test_core.py
@@ -1,5 +1,7 @@
 import dlt
 
+from dlt_mcp._tools.core import get_table_schema_changes
+
 @dlt.resource(table_name="users")
 def user_data(updated_user: bool):
     if updated_user:
@@ -10,18 +12,40 @@ def user_data(updated_user: bool):
     yield [
             {"id": 1, "name": "Alice", "email": "alice@example.com"},
             {"id": 2, "name": "Bob", "email": "bob@example.com"} 
-        ] 
-
+        ]
+    
 
 def test_get_table_schema_changes_when_schema_has_changed():
     pipeline_name = "table_schema_change_pipeline"
     pipeline = dlt.pipeline(
         pipeline_name,
-        destination="duckdb"
+        destination="duckdb",
+        dev_mode=True
     )
 
     pipeline.run(user_data(False))
     pipeline.run(user_data(True))
 
-    assert pipeline.schemas
-
+    expected_diff_message = """--- Current Schema
++++ Previous Schema
+@@ -6,6 +6,7 @@
+              '_dlt_load_id': {'data_type': 'text',
+                               'name': '_dlt_load_id',
+                               'nullable': False},
++             'age': {'data_type': 'bigint', 'name': 'age', 'nullable': True},
+              'email': {'data_type': 'text', 'name': 'email', 'nullable': True},
+              'id': {'data_type': 'bigint', 'name': 'id', 'nullable': True},
+              'name': {'data_type': 'text', 'name': 'name', 'nullable': True}},
+    """
+    
+    diff = get_table_schema_changes(pipeline_name, "users")
+    # Print actual and expected diff for readability in case of failure
+    assert diff.strip() == expected_diff_message.strip(), f"""
+    Expected and actual schema differences do not match.
+    
+    Expected:
+    {expected_diff_message.strip()}
+    
+    Actual:
+    {diff.strip()}
+    """
